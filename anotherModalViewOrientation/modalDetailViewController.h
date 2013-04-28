64de1873b3c9e338c92b492e2f847f20aa75a707
Andrey Derevyagin <2derand@gmail.com>
2013-04-28 13:57:09 +0300

    add details view controller to modal view controller

---
+++ anotherModalViewOrientation/modalDetailViewController.h
@@ -0,0 +1,13 @@
+//
+//  modalDetailViewController.h
+//  anotherModalViewOrientation
+//
+//  Created by Andrey Derevyagin on 4/28/13.
+//  Copyright (c) 2013 derand. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface modalDetailViewController : UIViewController
+
+@end
---
+++ anotherModalViewOrientation/modalDetailViewController.m
@@ -0,0 +1,38 @@
+//
+//  modalDetailViewController.m
+//  anotherModalViewOrientation
+//
+//  Created by Andrey Derevyagin on 4/28/13.
+//  Copyright (c) 2013 derand. All rights reserved.
+//
+
+#import "modalDetailViewController.h"
+
+@interface modalDetailViewController ()
+
+@end
+
+@implementation modalDetailViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
